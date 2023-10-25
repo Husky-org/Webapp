6560c59b0d571ea10171e0e2df3ffdbc2a5fcafa
mrkenkre <44857610+mrkenkre@users.noreply.github.com>
2023-10-25 04:12:24 -0400

    Feature/node4 (#54)
    * Updated workflows and systemd config.
    
    * Updated workflows and systemd config.
    
    * Updated workflows and systemd config.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,11 +13,12 @@ sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
 cd /opt/csye6225
 sudo unzip webapp.zip
 sudo chown -R csye6225:csye6225 /opt/csye6225
+sudo chmod +x /opt/csye6225/app.js
 
 sudo npm install
 
 sudo cp csye6225.service /etc/systemd/system
-sudo systemctl daemon-reloaddev
+sudo systemctl daemon-reload
 sudo systemctl enable csye6225
 sudo systemctl start csye6225
 sudo systemctl restart csye6225
